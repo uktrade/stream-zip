c83c3c01a5269a65b2a05dc87953172d1afe62fa
Michal Charemza <michal@charemza.name>
2021-12-29 14:34:08 +0000

    feat: even if passed all data in a single string, don't compress all at once

--- stream_zip.py
+++ stream_zip.py
@@ -4,6 +4,33 @@ import zlib
 
 def stream_zip(files, chunk_size=65536):
 
+    def evenly_sized(chunks):
+        chunk = b''
+        offset = 0
+        it = iter(chunks)
+
+        def up_to(num):
+            nonlocal chunk, offset
+
+            while num:
+                if offset == len(chunk):
+                    try:
+                        chunk = next(it)
+                    except StopIteration:
+                        break
+                    else:
+                        offset = 0
+                to_yield = min(num, len(chunk) - offset)
+                offset = offset + to_yield
+                num -= to_yield
+                yield chunk[offset - to_yield:offset]
+
+        while True:
+            block = b''.join(up_to(chunk_size))
+            if not block:
+                break
+            yield block
+
     def get_zipped_chunks_uneven():
         local_header_signature = b'PK\x03\x04'
         data_descriptor_signature = b'PK\x07\x08'
@@ -60,7 +87,7 @@ def stream_zip(files, chunk_size=65536):
             compressed_size = 0
             crc_32 = zlib.crc32(b'')
             compress_obj = zlib.compressobj(wbits=-zlib.MAX_WBITS, level=9)
-            for chunk in chunks:
+            for chunk in evenly_sized(chunks):
                 uncompressed_size += len(chunk)
                 crc_32 = zlib.crc32(chunk, crc_32)
                 compressed_chunk = compress_obj.compress(chunk)
@@ -147,32 +174,5 @@ def stream_zip(files, chunk_size=65536):
             0,           # ZIP file comment length
         ))
 
-    def get_zipped_chunks_even(zipped_chunks):
-        chunk = b''
-        offset = 0
-        it = iter(zipped_chunks)
-
-        def up_to(num):
-            nonlocal chunk, offset
-
-            while num:
-                if offset == len(chunk):
-                    try:
-                        chunk = next(it)
-                    except StopIteration:
-                        break
-                    else:
-                        offset = 0
-                to_yield = min(num, len(chunk) - offset)
-                offset = offset + to_yield
-                num -= to_yield
-                yield chunk[offset - to_yield:offset]
-
-        while True:
-            block = b''.join(up_to(chunk_size))
-            if not block:
-                break
-            yield block
-
     zipped_chunks = get_zipped_chunks_uneven()
-    yield from get_zipped_chunks_even(zipped_chunks)
+    yield from evenly_sized(zipped_chunks)
