cb83652ae0f8a1e83cfaed0f690436a9b24b851a
Michal Charemza <michal@charemza.name>
2021-12-29 12:27:22 +0000

    feat: start of central directory header

--- stream_zip.py
+++ stream_zip.py
@@ -9,17 +9,17 @@ def stream_zip(files, chunk_size=65536):
         data_descriptor_signature = b'PK\x07\x08'
         directory_header_signature = b'\x50\x4b\x01\x02'
         zip64_size_signature = b'\x01\x00'
-        local_header_struct = Struct('<H2sHHHIIIHH')
+        file_header_struct = Struct('<H2sHHHIIIHH')
         directory = []
 
         for name, modified_at, chunks in files:
             name_encoded = name.encode()
-            extra = \
+            local_extra = \
                 zip64_size_signature + \
                 Struct('<H').pack(16) + \
                 Struct('<QQ').pack(0, 0)  # Compressed and uncompressed sizes, 0 since data descriptor
             yield local_header_signature
-            yield local_header_struct.pack(
+            yield file_header_struct.pack(
                 45,                 # Version
                 b'\x08\x00',        # Flags - data descriptor
                 8,                  # Compression - deflate
@@ -29,10 +29,10 @@ def stream_zip(files, chunk_size=65536):
                 4294967295,         # Compressed size - since zip64
                 4294967295,         # Uncompressed size - since zip64
                 len(name_encoded),
-                len(extra),
+                len(local_extra),
             )
             yield name_encoded
-            yield extra
+            yield local_extra
 
             uncompressed_size = 0
             compressed_size = 0
@@ -53,12 +53,29 @@ def stream_zip(files, chunk_size=65536):
             yield data_descriptor_signature
             yield Struct('<IQQ').pack(crc_32, compressed_size, uncompressed_size)
 
-            directory.append((name_encoded, modified_at))
+            directory.append((name_encoded, modified_at, compressed_size, uncompressed_size))
 
-        for name, modified_at in directory:
+        for name, modified_at, compressed_size, uncompressed_size in directory:
             yield directory_header_signature
-            yield name
-            yield str(modified_at).encode()
+            directory_extra = \
+                zip64_size_signature + \
+                Struct('<H').pack(16) + \
+                Struct('<QQ').pack(compressed_size, uncompressed_size)
+            yield local_header_signature
+            yield file_header_struct.pack(
+                45,                 # Version
+                b'\x08\x00',        # Flags - data descriptor
+                8,                  # Compression - deflate
+                0,                  # Modification time
+                0,                  # Modification date
+                0,                  # CRC32 - 0 since data descriptor
+                4294967295,         # Compressed size - since zip64
+                4294967295,         # Uncompressed size - since zip64
+                len(name_encoded),
+                len(directory_extra),
+            )
+            yield name_encoded
+            yield directory_extra
 
     def get_zipped_chunks_even(zipped_chunks):
         chunk = b''
