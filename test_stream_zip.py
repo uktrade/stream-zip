f7cc1223051a7babc23edb94d9f06e71b7071682
Michal Charemza <michal@charemza.name>
2021-12-30 13:20:06 +0000

    feat: support no compression
    This is specifically for ODS files, since the mimetype file is meant to
    be first in the ZIP, and not compressed

--- test_stream_zip.py
+++ test_stream_zip.py
@@ -6,7 +6,7 @@ from zipfile import ZipFile
 import pytest
 from stream_unzip import stream_unzip
 
-from stream_zip import stream_zip
+from stream_zip import stream_zip, NO_COMPRESSION, ZIP64
 
 
 def test_with_stream_unzip():
@@ -14,10 +14,24 @@ def test_with_stream_unzip():
     perms = 0o600
 
     def files():
-        yield 'file-1', now, perms, (b'a' * 10000, b'b' * 10000)
-        yield 'file-2', now, perms, (b'c', b'd')
+        yield 'file-1', now, perms, ZIP64, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2', now, perms, ZIP64, (b'c', b'd')
 
     assert [(b'file-1', None, b'a' * 10000 + b'b' * 10000), (b'file-2', None, b'cd')] == [
+        (name, size, b''.join(chunks))
+        for name, size, chunks in stream_unzip(stream_zip(files()))
+    ]
+
+
+def test_with_stream_unzip_without_compresion():
+    now = datetime.fromisoformat('2021-01-01 21:01:12')
+    perms = 0o600
+
+    def files():
+        yield 'file-1', now, perms, NO_COMPRESSION, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2', now, perms, NO_COMPRESSION, (b'c', b'd')
+
+    assert [(b'file-1', 20000, b'a' * 10000 + b'b' * 10000), (b'file-2', 2, b'cd')] == [
         (name, size, b''.join(chunks))
         for name, size, chunks in stream_unzip(stream_zip(files()))
     ]
@@ -33,7 +47,7 @@ def test_with_stream_unzip_large():
             for i in range(0, 10000):
                 yield batch
 
-        yield 'file-1', now, perms, data()
+        yield 'file-1', now, perms, ZIP64, data()
 
     num_received = 0
     for name, size, chunks in stream_unzip(stream_zip(files())):
@@ -48,8 +62,43 @@ def test_with_zipfile():
     perms = 0o600
 
     def files():
-        yield 'file-1', now, perms, (b'a' * 10000, b'b' * 10000)
-        yield 'file-2', now, perms, (b'c', b'd')
+        yield 'file-1', now, perms, ZIP64, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2', now, perms, ZIP64, (b'c', b'd')
+
+    def extracted():
+        with ZipFile(BytesIO(b''.join(stream_zip(files())))) as my_zip:
+            for my_info in my_zip.infolist():
+                with my_zip.open(my_info.filename) as my_file:
+                    yield (
+                        my_info.filename,
+                        my_info.file_size,
+                        my_info.date_time,
+                        my_info.external_attr,
+                        my_file.read(),
+                    )
+
+    assert [(
+        'file-1',
+        20000,
+        (2021, 1, 1, 21, 1, 12),
+        perms << 16,
+        b'a' * 10000 + b'b' * 10000,
+    ), (
+        'file-2',
+        2,
+        (2021, 1, 1, 21, 1, 12),
+        perms << 16,
+        b'cd',
+    )] == list(extracted())
+
+
+def test_with_zipfile_without_compression():
+    now = datetime.fromisoformat('2021-01-01 21:01:12')
+    perms = 0o600
+
+    def files():
+        yield 'file-1', now, perms, NO_COMPRESSION, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2', now, perms, NO_COMPRESSION, (b'c', b'd')
 
     def extracted():
         with ZipFile(BytesIO(b''.join(stream_zip(files())))) as my_zip:
@@ -83,8 +132,8 @@ def test_directory_zipfile():
     perms = 0o600
 
     def files():
-        yield 'file-1', now, perms, (b'a' * 10000, b'b' * 10000)
-        yield 'file-2/', now, perms, ()
+        yield 'file-1', now, perms, ZIP64, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2/', now, perms, ZIP64, ()
 
     def extracted():
         with ZipFile(BytesIO(b''.join(stream_zip(files())))) as my_zip:
@@ -118,7 +167,7 @@ def test_exception_propagates():
     perms = 0o600
 
     def files():
-        yield 'file-1', now, perms, (b'a' * 10000, b'b' * 10000)
+        yield 'file-1', now, perms, ZIP64, (b'a' * 10000, b'b' * 10000)
         raise Exception('From generator')
 
     with pytest.raises(Exception,  match='From generator'):
@@ -130,7 +179,7 @@ def test_chunk_sizes():
     perms = 0o600
 
     def files():
-        yield 'file-1', now, perms, (os.urandom(1000000),)
+        yield 'file-1', now, perms, ZIP64, (os.urandom(1000000),)
 
     def get_sizes():
         for chunk in stream_zip(files()):
