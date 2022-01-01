041fee040bcfc9a70015c7aad91acaa7d72c95a3
Michal Charemza <michal@charemza.name>
2022-01-01 07:02:37 +0000

    tests: test with both zip and zip64

--- test_stream_zip.py
+++ test_stream_zip.py
@@ -37,6 +37,20 @@ def test_with_stream_unzip_zip():
     ]
 
 
+def test_with_stream_unzip_zip_and_zip64():
+    now = datetime.fromisoformat('2021-01-01 21:01:12')
+    perms = 0o600
+
+    def files():
+        yield 'file-1', now, perms, ZIP64, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2', now, perms, ZIP, (b'c', b'd')
+
+    assert [(b'file-1', None, b'a' * 10000 + b'b' * 10000), (b'file-2', None, b'cd')] == [
+        (name, size, b''.join(chunks))
+        for name, size, chunks in stream_unzip(stream_zip(files()))
+    ]
+
+
 def test_with_stream_unzip_without_compresion():
     now = datetime.fromisoformat('2021-01-01 21:01:12')
     perms = 0o600
@@ -112,6 +126,41 @@ def test_with_zipfile_zip():
 
     def files():
         yield 'file-1', now, perms, ZIP, (b'a' * 10000, b'b' * 10000)
+        yield 'file-2', now, perms, ZIP, (b'c', b'd')
+
+    def extracted():
+        with ZipFile(BytesIO(b''.join(stream_zip(files())))) as my_zip:
+            for my_info in my_zip.infolist():
+                with my_zip.open(my_info.filename) as my_file:
+                    yield (
+                        my_info.filename,
+                        my_info.file_size,
+                        my_info.date_time,
+                        my_info.external_attr,
+                        my_file.read(),
+                    )
+
+    assert [(
+        'file-1',
+        20000,
+        (2021, 1, 1, 21, 1, 12),
+        perms << 16,
+        b'a' * 10000 + b'b' * 10000,
+    ), (
+        'file-2',
+        2,
+        (2021, 1, 1, 21, 1, 12),
+        perms << 16,
+        b'cd',
+    )] == list(extracted())
+
+
+def test_with_zipfile_zip_and_zip64():
+    now = datetime.fromisoformat('2021-01-01 21:01:12')
+    perms = 0o600
+
+    def files():
+        yield 'file-1', now, perms, ZIP64, (b'a' * 10000, b'b' * 10000)
         yield 'file-2', now, perms, ZIP, (b'c', b'd')
 
     def extracted():
