869a825f3fc8d122c871ad662f017bdf50679a79
Michal Charemza <michal@charemza.name>
2024-02-18 13:34:12 +0000

    docs: add links to footer
    Add links to both the GitHub repo, and the gov.uk page for the department. This
    is to give the page a little bit more context, easier navigation to the repo,
    and a hint as to what the DBT at the top means.

--- eleventy.config.js
+++ eleventy.config.js
@@ -12,6 +12,20 @@ module.exports = function(eleventyConfig) {
         html: fs.readFileSync('./docs/assets/dit-logo.svg', {encoding: 'utf8'})
       },
       productName: 'stream-zip',
+    },
+    footer: {
+      meta: {
+        items: [
+          {
+            href: 'https://github.com/uktrade/stream-zip',
+            text: 'GitHub repository for stream-zip'
+          },
+          {
+            href: 'https://www.gov.uk/government/organisations/department-for-business-and-trade',
+            text: 'Created by the Department for Business and Trade (DBT)'
+          }
+        ]
+      }
     }
   })
 
